afb3fd4808438275c351f07560dc27188ebd5bca
Veamae Terrible <veamaeterrible.c@gmail.com>
2025-11-12 11:36:51 +0800

    Initial commit:)

---
+++ FaithMetrics/src/main/java/com/mycompany/faithmetrics/FaithMetrics.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.faithmetrics;
+
+/**
+ *
+ * @author veamae terrible
+ */
+public class FaithMetrics {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        
+    }
+}
